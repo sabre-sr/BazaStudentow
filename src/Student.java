1797a99edd1fb5a4dad5cd8af817038e56d8ef33
sabre183 <djsoundnr1@gmail.com>
2020-06-09 13:35:32 +0200

    db layout

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,6 @@
 public class Student extends Osoba {
     private String[] pesel;
-    private int[] nralbumu;
+    private int nralbumu;
     private int rok_studiow;
 
     public Student(String imienazwisko) {
