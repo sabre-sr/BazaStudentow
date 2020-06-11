c31e3f8151a76d3a9e8f28a74bf4de2467ce096e
sabre183 <djsoundnr1@gmail.com>
2020-06-11 19:40:43 +0200

    adding grades works, getting grade list works

--- src/BazaDanych.java
+++ src/BazaDanych.java
@@ -1,10 +1,12 @@
-import javax.xml.transform.Result;
 import java.sql.*;
-import java.util.HashMap;
-import org.apache.commons.lang3.tuple.ImmutablePair
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.text.WordUtils;
 
 public class BazaDanych {
     private Connection conn;
+
     private BazaDanych() {
         Connection conn = null;
         String path = "jdbc:sqlite:baza.db";
@@ -35,20 +37,52 @@ public class BazaDanych {
         PreparedStatement ps = conn.prepareStatement("SELECT * FROM studenci WHERE (?=?)");
         if (s.getNralbumu() != 0) {
             ps.setInt(1, s.getNralbumu());
-        }
-        else if (!s.getImieNazwisko().equals(""))
+        } else if (!s.getImieNazwisko().equals(""))
             ps.setString(1, s.getImieNazwisko());
         ResultSet wynik = ps.executeQuery();
         return wynik;
     }
 
-    public ImmutablePair<String, String> getGrades (int student_id) throws SQLException {
+    public ArrayList<ImmutablePair<String, String>> getGrades(int student_id) throws SQLException {
         PreparedStatement ps = conn.prepareStatement("SELECT * FROM przedmioty");
         ResultSet przedmioty = ps.executeQuery();
+        ArrayList<ImmutablePair<String, String>> results = new ArrayList<>();
+        while (przedmioty.next()) {
+            String przedmiot = przedmioty.getString("nazwatabeli");
+            System.out.println(przedmiot);
+            String query = String.format("SELECT * FROM %s WHERE (id_stud = ?)", przedmiot);
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, student_id);
+            ResultSet oceny = ps.executeQuery();
+            oceny.next();
+            results.add(new ImmutablePair<>(przedmioty.getString("nazwa"), oceny.getString("oceny")));
+            continue;
+        }
+        return null;
     }
 
-    public static void main(String[] args) {
-        new BazaDanych();
+    public void addPrzedmiot(String przedmiot) throws SQLException {
+        String tabelanazwa = WordUtils.capitalizeFully(przedmiot, ' ').replaceAll(" ", "");
+        String sql = String.format("create table %s\n" +
+                "(\n" +
+                "    id      INTEGER\n" +
+                "        constraint %s\n" +
+                "            primary key autoincrement,\n" +
+                "    id_stud int not null\n" +
+                "        references studenci\n" +
+                "            on update cascade on delete cascade,\n" +
+                "    oceny   text\n" +
+                ");", tabelanazwa, (tabelanazwa + "_pk"));
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.execute();
+        ps = conn.prepareStatement("INSERT INTO przedmioty(nazwa, nazwatabeli) VALUES (?, ?)");
+        ps.setString(1, przedmiot);
+        ps.setString(2, tabelanazwa);
+        ps.execute();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        new BazaDanych().addPrzedmiot("Nazwa testowa");
     }
 
 }
