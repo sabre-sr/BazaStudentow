005fa92d75d72016c1cf71ad9b6810b17872b0c7
sabre183 <djsoundnr1@gmail.com>
2020-06-16 23:24:28 +0200

    its coming together so much done i dont keep track

--- src/main/java/GUI/StudentManagement.java
+++ src/main/java/GUI/StudentManagement.java
@@ -1,4 +1,26 @@
 package GUI;
 
-public class StudentManagement {
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+
+public class StudentManagement extends JFrame {
+    private JPanel tabelka;
+    private final JButton znajdz, dodaj, edytuj, karta;
+    public StudentManagement () throws SQLException {
+        super("Zarządzanie studentami");
+        this.add(this.tabelka = new StudentLista());
+        this.add(this.dodaj = new JButton("Dodaj"));
+        this.add(this.znajdz = new JButton("Znajdź"));
+        this.add(this.edytuj = new JButton("Edytuj"));
+        this.add(this.karta = new JButton("Karta studenta"));
+        this.setLayout(new FlowLayout());
+        this.setSize(470,520);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        new StudentManagement();
+    }
 }
